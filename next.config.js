9af24a6e9e7261bb53864c9387ca13195819a6a5
Aurish Hammad Hafeez <aurishhammad@gmail.com>
2025-06-24 20:26:13 +0500

    Fix GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/AIForTEENS' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/AIForTEENS/' : '',
+  }
 }
 
 module.exports = nextConfig 
